75af45eed78a2d857a2f0566138a000762cc5587
Emily Resch <eresch527@gmail.com>
2020-04-24 11:14:14 -0500

    added color

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ var express = require("express");
 var app = express();
 var path = require("path");
 var PORT = process.env.PORT || 8080;
+
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
+
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"))
